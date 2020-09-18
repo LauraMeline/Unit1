901ea210fdb998ec368f9ef19e5a925805b9a42d
LauraMeline <821219@mystma.org>
2020-09-18 18:29:24 +0000

    finished assignment

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,11 +1,25 @@
 package app;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Harry Potter Series");
-        System.out.println("Hunger Games Series");
-        System.out.println("Original Mulan");
-        System.out.println("Star Wars Series");
-        System.out.println("Big Hero 6");
+    public static void main(final String[] args) throws Exception {
+        boolean likePie = true;
+        Byte brosFavNum = 29;
+        short songsOfPlaylist = 156;
+        int studentId = 821219;
+        long moneyOfTaySwift = 360000000;
+
+    
+        
+        
+        System.out.println("Do I like pie");
+        System.out.println(likePie);
+        System.out.println("What is my brothers favorite number");
+        System.out.println(brosFavNum);
+        System.out.println("How many songs are in my playlist");
+        System.out.println(songsOfPlaylist);
+        System.out.println("What is my student ID");
+        System.out.println(studentId);
+        System.out.println("how much money is taylor swift worth");
+        System.out.println(moneyOfTaySwift);
     }
-}
+}    
